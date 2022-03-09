f03d579c3b3c0cd1049e02e0b3c5d6f9cbfd408a
cynthias13w <mcynsiew@gmail.com>
2022-03-09 11:00:12 +0100

    deleted files

--- encoder.py
+++
@@ -1,8 +0,0 @@
-from sklearn.compose import ColumnTransformer
-import pandas as pd
-
-class CustomColumnTransformer(ColumnTransformer):
-    def transform(self, *args, **kwargs):
-        return pd.DataFrame(super().transform(*args, **kwargs), columns = self.get_feature_names_out())
-    def fit_transform(self, *args, **kwargs):
-        return pd.DataFrame(super().fit_transform(*args, **kwargs), columns = self.get_feature_names_out())
